bc15ad80ba93ecc780fab86939166270808a183a
resulyag <resulyag@gmail.com>
2021-04-05 00:05:07 +0300

    firebase eklendi
    bildirim gönderimi sağlandı
    firebase tarafında proje oluşturuldu

--- app/src/main/java/com/resulyag/misafirapp/MainActivity.java
+++ app/src/main/java/com/resulyag/misafirapp/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        runtimeEnableAutoInit();
     }
+
+    public void runtimeEnableAutoInit() {
+        // [START fcm_runtime_enable_auto_init]
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+        // [END fcm_runtime_enable_auto_init]
+    }
+
 }
